76cfd5fe59d486484320fe53fafe4c7b6a063b39
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2015-12-19 19:24:29 -0500

    added animation and transitions

--- app/directives/arcLogo.js
+++
@@ -1,9 +0,0 @@
-app.directive('arcLogo', function () {
-  var link = function (scope, element, attrs) {
-    $(element).arctext({radius: 400});
-  };
-  return {
-    restrict: 'A',
-    link: link
-  };
-});
---
+++ app/directives/welLogo.js
@@ -0,0 +1,14 @@
+app.directive('welLogo', function () {
+  var link = function (scope, element, attrs) {
+    var curved = angular.element(element.children()[0]);
+    var robotics = angular.element(element.children()[2]);
+    $(curved).arctext({radius: 400});
+    $(robotics).lettering();
+    $(element).find('span').addClass('shake-slow');
+  };
+  return {
+    templateUrl: 'app/directives/wel-logo.html',
+    restrict: 'E',
+    link: link
+  };
+});
