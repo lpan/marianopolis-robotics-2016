fd866bae94755e31d87b24a2ed5491bb73aaf611
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-08 14:41:18 -0500

    almost finished talking mushroom

--- app/components/info/talking-mushroom/talkingMushroom.js
+++ app/components/info/talking-mushroom/talkingMushroom.js
@@ -15,7 +15,7 @@ app.directive('talkingMushroom', ['languageService', '$timeout', function (langu
     // update text according to counter
     scope.counter = 0;
     scope.next = function () {
-      if (scope.counter !== intro.length-1)
+      if (scope.counter !== intro.length)
         scope.counter ++;
     };
     scope.previous = function () {
@@ -24,28 +24,60 @@ app.directive('talkingMushroom', ['languageService', '$timeout', function (langu
     };
     var goAhead = function () {
       scope.text = intro[scope.counter];
-      speech.html(scope.text);
+      speech.addClass('animated fadeOut');
+      $timeout(function () {
+        speech.html(scope.text);
+        speech.removeClass('animated fadeOut');
+        speech.addClass('animated fadeIn');
+      }, 500);
     };
+    // move mushroom to the corner
+    var moveHide = function () {
+      // hide dialog box and stuff
+      scope.moved = false;
+      dialog.addClass('animated fadeOut');
+      speech.addClass('animated fadeOut');
+      // move mushroom
+      mushroom.removeClass('mushroom-talk');
+      mushroom.addClass('mushroom-corner');
+    };
+
     scope.$watch('counter', function () {
-      goAhead();
+      if (scope.counter === intro.length-1) {
+        goAhead();
+        button2Text.html('Got it');
+        button2Text.addClass('animated swing');
+      } else if (scope.counter === intro.length) {
+        moveHide();
+      } else {
+        goAhead();
+        button2Text.html('Next');
+        button2Text.removeClass('animated swing');
+      }
     });
+
     var mushroom = $('.mushroom-container');
     var dialog = $('.speech-bubble');
     var speech = $('#mushroom-speech');
+    var button2Text = $('.flow-buttons > div:nth-child(2)');
     // hover effect
     mushroom.hover(function () {
-      mushroom.addClass('animated bounce');
+      mushroom.addClass('animated tada');
     }, function () {
-      mushroom.removeClass('animated bounce');
+      mushroom.removeClass('animated tada');
     });
+    // center the mushroom
     mushroom.addClass('mushroom-center');
     mushroom.addClass('animated bounceInUp');
     $timeout(function () {
+      // avoid conflict between css transition and animate.css
       mushroom.removeClass('animated bounceInUp');
+      mushroom.removeClass('mushroom-center');
       mushroom.addClass('mushroom-move');
       mushroom.addClass('mushroom-talk');
       dialog.addClass('dialog-position');
       $timeout(function () {
+        // when mushroom is positioned, show dialog
         goAhead();
         scope.moved = true;
         dialog.addClass('animated fadeIn');
