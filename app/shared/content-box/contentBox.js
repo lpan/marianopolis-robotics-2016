a5eadbd503a4bbd99823b93e53d61a71571529aa
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-10 17:07:08 -0500

    add social media directive

---
+++ app/shared/content-box/contentBox.js
@@ -0,0 +1,6 @@
+app.directive('contentBox', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/shared/content-box/content-box.html'
+  };
+});
---
+++ app/shared/social-media/socialMedia.js
@@ -0,0 +1,6 @@
+app.directive('socialMedia', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/shared/social-media/social-media.html'
+  };
+});
