53090546d0276f296d5aaf8a161e79ff1c975ad3
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-02-05 17:07:02 -0500

    fixed team buttons overlay issue

--- app/components/tutorials/article-nav/articleNav.js
+++ app/components/tutorials/article-nav/articleNav.js
@@ -1,6 +1,21 @@
 app.directive('articleNav', ['languageService', function (language) {
+
+  var controller = ['$scope', function ($scope) {
+    $scope.articles = [
+      {
+        name: "Translation with AngularJs",
+        author: "Lawrence Pan"
+      },
+      {
+        name: "Line Tracking Tutorial",
+        author: "Derek Yu, Bilun Sun"
+      }
+    ];
+  }];
+
   return {
     restrict: 'E',
+    controller: controller,
     templateUrl: 'app/components/tutorials/article-nav/article-nav.html'
   };
 }]);
