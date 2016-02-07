8903f1c2e2afb91c6f752c913516bd5a3a88073d
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-02-06 23:40:50 -0500

    now users are able to change language within app

--- app/services/languageService.js
+++ app/services/languageService.js
@@ -16,52 +16,7 @@ app.provider('languageService', function () {
     language.setLang = function (lang) {
       this.lang = lang;
     };
-    // alternate state of the switcher button
-    // lang => string
-    // btn => jqlite object
-    // new value
-    language.altButton = function (lang, btn1, btn2, newLang, activeClass, stillClass) {
-      if (newLang === lang) {
-        btn1.removeClass(stillClass);
-        btn1.addClass(activeClass);
-        btn2.removeClass(activeClass);
-        btn2.addClass(stillClass);
-      } else {
-        btn1.removeClass(activeClass);
-        btn1.addClass(stillClass);
-        btn2.removeClass(stillClass);
-        btn2.addClass(activeClass);
-      }
-    };
-    // In & out animation when switching languages
-    // elements is a NodeList of elements wished to be animated
-    // classIn and classOut are lists of classes in strings
-    // callback is the action before elements are faded in
-    language.animate = function (elements, classIn, classOut, time, callback) {
-      // add out classes
-      elements.forEach(function (element) {
-        element.addClass(classOut);
-      });
-      language.buttonClicked = true;
-      $timeout(function () {
-        if (typeof callback === 'function')
-          callback();
-        elements.forEach(function (element) {
-          element.removeClass(classOut);
-          element.addClass(classIn);
-        });
-        language.buttonClicked = false;
-      }, time);
-    };
-    // helpers
-    // nodelist from querySelectorAll to list of dom objects
-    language.toList = function (nodeList) {
-      var list = [];
-      for (var i = 0; i < nodeList.length; i++) {
-        list.push(angular.element(nodeList[i]));
-      }
-      return list;
-    };
+    language.currentPage = '/';
     return language;
   }]);
 });
--- app/shared/translation/ch-lang/chLang.js
+++ app/shared/translation/ch-lang/chLang.js
@@ -1,32 +1,26 @@
-app.directive('chLang', ['languageService', '$timeout', function (language, $timeout) {
-  var link = function (scope, element, attrs) {
-    var en = angular.element(element.find('span')[0]);
-    var fr = angular.element(element.find('span')[1]);
-    var classes = [en, fr];
-    // nodeList to list of dom objects
-    // language.toList(document.querySelectorAll('.translate'), classes);
-    scope.setLang = function (lang) {
-      // no effect if active button is clicked
-      if (language.lang === lang) {
-        return;
+app.directive('chLang', [
+  '$route',
+  'languageService',
+  function ($route, language) {
+    var controller = ['$scope', function ($scope) {
+      var targetLang;
+      if (language.lang === 'en') {
+        $scope.text = 'En Français';
+        targetLang = 'fr';
       } else {
-        // fadein, change language.buttonClicked
-        // wait 800ms, change language.lang, fadein
-        language.animate(classes, 'animated fadeIn', 'animated fadeOut', 800, function () {
-          language.lang = lang;
-        });
+        $scope.text = 'To English';
+        targetLang = 'en';
       }
+      $scope.translate = function () {
+        language.lang = targetLang;
+        $route.reload();
+      };
+    }];
+    return {
+      restrict: 'E',
+      templateUrl: 'app/shared/translation/ch-lang/ch-lang.html',
+      controller: controller,
+      scope: true
     };
-    // according to the current lang, assign state of lang button
-    scope.$watch(function () {
-      return language.lang;
-    }, function () {
-      scope.lang = language.lang;
-    });
-  };
-  return {
-    restrict: 'E',
-    templateUrl: 'app/shared/translation/ch-lang/ch-lang.html',
-    link: link
-  };
-}]);
+  }
+]);
